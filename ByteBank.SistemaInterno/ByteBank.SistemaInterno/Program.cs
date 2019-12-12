eb245accf070a24b5033c0e6bf5989cd7f747d79
afranca <afranca@magnasistemas.com.br>
2019-12-12 10:16:58 -0300

    Projeto final C# parte 5

---
+++ ByteBank.SistemaInterno/ByteBank.SistemaInterno/Program.cs
@@ -0,0 +1,24 @@
+﻿using ByteBank.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank.SistemaInterno
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente conta = new ContaCorrente(34534,23423);
+            Console.WriteLine(conta.Saldo);
+
+            string nome = "Angelica";
+
+            
+
+            Console.ReadLine();
+        }
+    }
+}

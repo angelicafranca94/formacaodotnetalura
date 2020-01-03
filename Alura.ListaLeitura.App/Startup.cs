4801e5c5381c8afb2a43b2cb8ec30e95d2c11bcc
afranca <afranca@MGNOTE0757>
2020-01-03 09:15:25 -0300

    Projeto Final Asp NET CORE

---
+++ Alura.ListaLeitura.App/Startup.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Alura.ListaLeitura.App
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            //var builder = new RouteBuilder(app);
+
+            //builder.MapRoute("{controller}/{action}", RoteamentoPadrao.TratamentoPadrao);
+
+            //builder.MapRoute("Livros/ParaLer", LivrosLogica.ParaLer);
+            //builder.MapRoute("Livros/Lendo", LivrosLogica.Lendo);
+            //builder.MapRoute("Livros/Lidos", LivrosLogica.Lidos);
+            //builder.MapRoute("Livros/Detalhes/{id:int}", LivrosLogica.Detalhes);
+            //builder.MapRoute("Cadastro/NovoLivro/{nome}/{autor}", CadastroLogica.NovoLivro);
+            //builder.MapRoute("Cadastro/ExibirFomulario", CadastroLogica.ExibirFomulario);
+            //builder.MapRoute("Cadastro/Incluir", CadastroLogica.Incluir);
+
+            //var rotas = builder.Build();
+
+            //app.UseRouter(rotas);
+
+
+            app.UseDeveloperExceptionPage();
+
+            app.UseMvcWithDefaultRoute();
+
+        }
+
+       
+
+       
+    }
+}
